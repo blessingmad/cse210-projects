aacf6413c6213f3ba593e2d648ce836de49b1ad9
blessingmad <mad22008@byui.edu>
2023-01-14 00:54:18 +0200

    magic number

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,74 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //DisplayWelcomeMessage
+
+        //store user name
+        string userName = AskUserName();
+        //store user number
+        int userNumber = AskUserNumber();
+
+
+        int squredNumber = squreNumber(userNumber);
+
+        DisplayResults(userName, squredNumber);
+    {
+    //static void DisplayWelcomeMessage()
     }
-}
+        // write to the terminal
+        Console.WriteLine("Wecome to the Program");
+    }
+
+    //ask user name function
+    static string AskUserName()
+    { 
+        //display to the window
+        Console.Write("What is your name:");
+        //get the respond from the user
+        string name = Console.ReadLine();
+
+        //keeps the user input
+        return name;
+    }
+    //ask user number function
+    static int AskUserNumber()
+
+    {
+        //write to the window
+        Console.Write("Please enter your magic number:");
+        // read from the window
+        int number = int.Parse(Console.ReadLine());
+
+
+        //keep the answer from the user
+        return number;
+    }
+
+    // get squre function with int number as a parameter.
+     static int squreNumber(int number)
+     {
+        //compute the squre
+        int squre = number * number;
+        // keep the number
+        return squre;
+     }
+
+     // Shows name and number to the user.
+    static void DisplayResults(string name, int squre)
+    {
+        Console.WriteLine($"{name}, the power of your number is {squre}");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+} 
