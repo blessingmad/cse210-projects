c6f3112bac8207636c6fa73e32ef8aa737bbd4a2
blessingmad <mad22008@byui.edu>
2023-01-18 23:41:03 +0200

    week 3 practice

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,19 +1,20 @@
 using System;
 
-
 public class Job
 {   public string _jobTitle ;
     public string _jobCompany ;
     public int _startYear ;
     public int _endYear;
 
-
+    public string _name;
+    public string _jobs;
     public void Display()
         {
-            Console.WriteLine($"{_jobTitle} {_jobCompany} {_startYear} {_endYear}");
+            Console.WriteLine($"{_jobTitle} ({_jobCompany}){_startYear}  - {_endYear}");
+            Console.WriteLine();
         }
+
     
 }
-
 
 
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,7 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._jobCompany = "Microsoft";
+        job1._startYear = 2845;
+        job1._endYear = 2848;
         
+        //create another job portion
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._jobCompany = "Samsung";
+        job2._startYear = 2851;
+        job2._endYear = 2855; 
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Moipone Palesa";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+
+
+
+
     }
 
 }
