8d37f1656631bbffaa2601f8a09cea04c00f3a2f
blessingmad <mad22008@byui.edu>
2023-02-11 18:10:29 +0200

    journal update

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -2,16 +2,16 @@ using System;
 class   Program
 {
     static void Main(string[] args)
-    {
-        Scripture scripture1 = new Scripture();
-        scripture1._reference = 3;
-
+    {   
         
-        scripture1._scripture = "For God so loved the world,that he gave his only begotten Son that whosoever believeth in him should not perish, but have everlasting life";
+        DateTime theCurrentTime = DateTime.Today;
+        string dateText = theCurrentTime.ToLongDateString();
+        Console.WriteLine($"{dateText}");
+
+        Console.Write("For God so loved the world,that he gave his only begotten Son that whosoever believeth in him should not perish, but have everlasting life");
+        string _scripture = Console.ReadLine();
 
         Reference myReference = new Reference();
-        myReference._book = "John";
-        myReference._chapter = 3;
-        myReference._verse  = 16;
+        
     }
 } 
